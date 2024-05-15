440cba935d91367bd14802911e21480294cfce54
ankr2063 <ankr2063@gmil.com>
2024-05-15 10:28:20 +0530

    topbar fixed

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -4,17 +4,16 @@ import { clear } from '../utils/Crypto'
 
 export default function TopBar() {
   return (
-    <nav className="navbar">
-        <div className="d-flex justify-content-between align-items-center grid-margin row-xs-12">
+    <nav className="navbar d-flex flex-column flex-md-row align-items-center justify-content-between">
 
           <div>
-            <h1 className="mb-3 mb-md-0">Welcome to Dashboard</h1>
+            <h1 className="mb-3 mb-md-0 ms-5">Welcome to Dashboard!</h1>
           </div>
 
           <div className="d-flex align-items-center">
             <button
               type="button"
-              className="btn btn-outline-primary btn-icon-text mb-2 mb-md-0"
+              className="btn btn-outline-primary btn-icon-text mb-2 mb-md-0 me-5"
               onClick={() => {
                 clear();
                 window.location = '/';
@@ -22,7 +21,6 @@ export default function TopBar() {
             >
               Logout
             </button>
-          </div>
       </div>
 
 
