4e342d898a753f1619b9506b2c6e99535dea148e
ankr2063 <ankr2063@gmil.com>
2024-06-03 10:23:54 +0530

    config setup

--- src/Network/Api/config.js
+++ src/Network/Api/config.js
@@ -8,7 +8,7 @@ if (DEVELOPMENT_MODE) {
   api = "http://127.0.0.1:3003/api/admin/";
   token = () => get("refresh_token");
 } else {
-  api = "https://dev.rideforyoutransport.com/api/admin";
+  api = "https://dev.rideforyoutransport.com/api/admin/";
   token = () => get("refresh_token");
 }
 
