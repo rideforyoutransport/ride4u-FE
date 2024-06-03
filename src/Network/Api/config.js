215af4401bd96d06e2e5c482f9baf2a9e6f1b5e8
ankr2063 <ankr2063@gmil.com>
2024-06-03 10:17:18 +0530

    prod base url changed

--- src/Network/Api/config.js
+++ src/Network/Api/config.js
@@ -1,14 +1,14 @@
 import { get } from "../../utils/Crypto";
 
-//let DEVELOPMENT_MODE = window.location.href.includes("localhost");
-let DEVELOPMENT_MODE = true;
+let DEVELOPMENT_MODE = window.location.href.includes("localhost");
+//let DEVELOPMENT_MODE = true;
 
 let api, token;
 if (DEVELOPMENT_MODE) {
   api = "http://127.0.0.1:3003/api/admin/";
   token = () => get("refresh_token");
 } else {
-  api = "https://preview.linebyline.live/api/admin/";
+  api = "https://dev.rideforyoutransport.com/api/admin";
   token = () => get("refresh_token");
 }
 
