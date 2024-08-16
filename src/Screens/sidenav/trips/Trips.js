b67aa1ed75880b30e5ba274c460492e9812b4e30
Sahil Yadav <sahilyadav@Sahils-MacBook-Air.local>
2024-08-17 02:21:08 +0530

    date locale fix

--- src/Screens/sidenav/trips/Trips.js
+++ src/Screens/sidenav/trips/Trips.js
@@ -3,6 +3,8 @@ import DataTable from "react-data-table-component";
 import { useNavigate } from "react-router-dom";
 import { post } from "../../../Network/Config/Axios";
 import { showToast } from "../../../utils/Toast";
+import dayjs from "dayjs";
+import moment from "moment";
 
 
 
@@ -98,7 +100,7 @@ export default function Trips() {
     },
     {
       name: "Trip Date",
-      selector: (row) => row.tripDate,
+      selector: (row) => new Date(row.tripDate).toLocaleString("en-US").toString(),
       sortable: true,
       index: 2
     },
